642f3ab4856e23243a4bb9176cfabdc9f9dd202a
kafpliz <kafpliz@gmail.com>
2024-10-07 22:17:19 +0300

    fix bugs 2214

--- src/app/pages/error/error.component.ts
+++ src/app/pages/error/error.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-error',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './error.component.scss'
 })
 export class ErrorComponent {
-
+  mes = input()
 }

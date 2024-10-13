307bf07d4d4ba4ae2b23c5f91b40f936ce93c58b
kafpliz <kafpliz@gmail.com>
2024-10-13 22:18:51 +0300

    add

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -4,11 +4,11 @@ import { EMovie } from '../../shared/enums/movie.enum';
 import { IMovie } from '../../data/interfaces/movie.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' 
 })
 export class MovieService extends BaseServiceService{
 
   getMovie(id:number){
-    return this.http.get<IMovie>(EMovie.url /* + `/${id}` */)
+    return this.http.get<IMovie>(EMovie.url + `/${id}` + `/${this.tg.getUserId()}`)
   }
 }
--- src/app/pages/main/components/top-movie/top-movie.component.ts
+++ src/app/pages/main/components/top-movie/top-movie.component.ts
@@ -2,11 +2,12 @@ import { CUSTOM_ELEMENTS_SCHEMA, Component, inject, input } from '@angular/core'
 import { ITopMovie } from '../../../../data/interfaces/top-movie.interface';
 import { RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { RoundToNearestPipe } from '../../../../core/pipes/round-to-nearest.pipe';
 
 @Component({
   selector: 'app-top-movie',
   standalone: true,
-  imports: [RouterLink,CommonModule],
+  imports: [RouterLink,CommonModule, RoundToNearestPipe],
   templateUrl: './top-movie.component.html',
   styleUrl: './top-movie.component.scss',
   schemas: [CUSTOM_ELEMENTS_SCHEMA],

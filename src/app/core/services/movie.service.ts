c128a9a4ec589450d7b48049646d0a96552db67a
kafpliz <kafpliz@gmail.com>
2025-01-29 19:43:27 +0300

    suka

--- src/app/core/services/movie.service.ts
+++ src/app/core/services/movie.service.ts
@@ -11,7 +11,7 @@ export class MovieService extends BaseServiceService {
   #filmId: number = 0;
   getMovie(id: number) {
     this.#filmId = id
-    return this.http.get<IMovie>(EMovie.url + `/${id}` + `/${/* this.tg.getUserId() */1805991962}`)
+    return this.http.get<IMovie>(EMovie.url + `/${id}` + `/${this.tg.getUserId()}`)
   }
 
   sendFilm(fl?: string) {
